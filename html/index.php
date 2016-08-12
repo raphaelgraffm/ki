6039e6ba23c95ed9fa559ac328497213763655c8
raphaelgraffm <raphgraff@live.fr>
2016-08-12 20:14:21 +0200

    Harmonise la direction des obliques

--- html/index.php
+++ html/index.php
@@ -1,19 +1,16 @@
 <h1 class="titre mi-court"><span>KI, Kesako?</span></h1>
 
 <div id="principal">
-<p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vel orci euismod, accumsan dui at, volutpat leo. Mauris bibendum leo quis auctor varius. Duis eget congue quam, quis ultrices est. Aliquam rutrum dui lacus, eu venenatis purus ornare id. Etiam in quam tortor. Vestibulum eu dui arcu. Curabitur tincidunt blandit tortor venenatis faucibus. Nunc faucibus felis interdum, commodo odio dictum, pellentesque dui. Vivamus mattis quis felis quis porttitor. Nunc lobortis libero vel nulla dictum congue.
-</p>
-<p>
-	Vestibulum eget erat laoreet odio venenatis porta ac et augue. Aliquam non tellus at massa egestas tincidunt. Aliquam vestibulum, lorem a mattis sodales, nulla velit maximus arcu, at sodales est massa in neque. Nullam eu malesuada sem, vitae maximus tellus. Mauris eu urna neque. Vivamus eros ex, aliquam nec hendrerit nec, sollicitudin ut nulla. Pellentesque elementum tortor felis, nec eleifend orci mattis non. Mauris ipsum dolor, tempus et dui faucibus, fermentum pulvinar odio. Vivamus ut turpis sit amet eros faucibus imperdiet. Nam scelerisque varius nibh, tincidunt accumsan metus dictum non. Pellentesque et ex in felis cursus dictum.
-</p>
-<p>
-	Fusce vitae nisi vel eros consequat placerat quis eget justo. Aenean rhoncus odio id eros tincidunt, a tempor enim pellentesque. Vestibulum et commodo mauris. Vivamus quis velit in orci ultrices viverra. Ut quis sem eu sapien luctus blandit. Mauris pellentesque, sem ac pretium rhoncus, ex massa porttitor magna, non varius enim turpis sit amet diam. Maecenas sollicitudin, tortor et ultricies venenatis, lacus odio lobortis lacus, eu vulputate justo tellus sit amet arcu. Phasellus eget enim venenatis, gravida erat at, malesuada risus. Cras elementum tempus lorem, non facilisis lacus congue eu. Morbi nisi odio, maximus at interdum dictum, dignissim posuere lorem.
-</p>
-<p>
-	Donec vulputate neque ut leo malesuada tristique. Nulla ligula tellus, feugiat et diam et, accumsan ornare dolor. Praesent augue dolor, aliquet sit amet placerat id, semper ac tortor. Fusce ex velit, tempor ut libero sit amet, vestibulum fringilla orci. Vivamus eget purus lectus. Proin id magna pretium, efficitur erat quis, pharetra sapien. Quisque vehicula tempus nisl, eu volutpat nulla eleifend in. Fusce sit amet imperdiet turpis, vel iaculis ligula. Morbi aliquet orci tellus, id consequat sapien iaculis quis. Mauris venenatis orci vitae maximus tristique. Fusce ultricies, elit a convallis vulputate, est diam accumsan ante, eget posuere lectus nunc sed tortor. Aenean vulputate magna turpis, quis accumsan metus ultrices sed. Donec iaculis arcu libero, vitae dignissim est euismod eget. Sed non nibh ut turpis venenatis vehicula. Curabitur fermentum felis id ante pharetra egestas.
-</p>
-<p>
-	Nam et dui ac elit molestie volutpat. Curabitur risus purus, eleifend nec malesuada at, fringilla sit amet ex. Fusce iaculis erat nec viverra mattis. Pellentesque et hendrerit ipsum. Vivamus sit amet dolor nec orci tempor pellentesque a vitae purus. Sed pharetra urna velit, non semper lacus sagittis eget. Quisque in vestibulum nunc, eget congue ligula. Curabitur semper risus sapien, nec placerat massa tempus sed. Maecenas interdum, ligula eu mattis sodales, ipsum est tincidunt urna, a molestie leo erat quis est. Etiam imperdiet nibh sit amet odio lacinia volutpat. Quisque laoreet, turpis sit amet maximus convallis, urna tellus maximus nunc, vitae finibus mauris mi a sem. Nullam eros nisi, facilisis ac consequat at, feugiat nec arcu.
-</p>
+	<p>
+		Qu'est-ce que le KI ? Eh bien, le KI (prononcer K-I) est le <span class="surligne">Club Informatique de l'École des Ponts ParisTech</span>. Fondé en 1997, le KI est un incontounable de la vie étudiante aux Ponts ! En effet nous touchons à tout ce qui se rapporte à l'informatique et ferons notre possible pour t'offrir une expérience virtuelle agréable et dynamique.
+	</p>
+	<p>
+		Ainsi, que tu veuilles jouer à des jeux vidéo (Wii-U et ordinateurs toujours prêts), en apprendre plus (grâce à nos formations), emprunter du matériel, ou résoudre un problème informatique, tu peux toujours pousser la porte de notre local !
+	</p>
+	<p>
+		Nous développons également, entre autres projets, l'intranet étudiant des Ponts. Nous nous ferons un plaisir de t'accueillir pour te faire découvrir nos activités, que tu puisses en profiter et si tu le souhaites t'y essayer au développement web !
+	</p>
+	<p>
+		Tu l'auras compris nous sommes motivés par le désir d'apprendre, le goût des nouvelles technologies et l'envie d'aider !
+	</p>
 </div>
