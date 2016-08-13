a3bff957fc2e84f011640721f6f25fd86d20a8ab
raphaelgraffm <raphgraff@live.fr>
2016-08-13 11:27:39 +0200

    Change l'apparence de la page Formations

--- html/formations.php
+++ html/formations.php
@@ -14,40 +14,36 @@
 		Pas de problème, le KI ne s’arrête pas là ! En plus de formations ponctuelles, le KI propose chaque année <span class="surligne">une formation continue et personnalisée</span>, à raison d’un rendez-vous par semaine. En 2016, nous avons pu apprendre les bases de la programmation Web à travers HTML, CSS, Javascript et le framework PHP Symfony 2, mais qui sait ce que le KI te réserve cette année ?
 	</p>
 
-	<div class="section-bleu">
+	<h2 class="sous-titre"><span>FORMATIONS PRECEDENTES</span></h2>
 
-		<h2 class="sous-titre">Nos précédentes formations</h2>
+	<div class="paragraphe clearfix">
+		<img class="float-right" src="img/accueil/local.jpg"/>
+		<p>
+			La <span class="surligne">Formation LaTeX</span> a permis à ceux qui tapaient jusqu'ici leurs rendus et leurs projets sur Microsoft Word de s'initier à cet outil puissant de mise en page. 
+		</p>
+		<p>
+			En deux heures, entrecoupées d'une pause pizza méritée, les élèves ont eu un aperçu des commandes les plus utiles et ont pu se faire la main sur quelques TP encadrés par les membres du club. 
+		</p>
+	</div>
 
-		<div class="paragraphe clearfix">
-			<img class="float-right" src="img/accueil/local.jpg"/>
-			<p>
-				La Formation LaTeX a permis à ceux qui tapaient jusqu'ici leurs rendus et leurs projets sur Microsoft Word de s'initier à cet outil puissant de mise en page. 
-			</p>
-			<p>
-				En deux heures, entrecoupées d'une pause pizza méritée, les élèves ont eu un aperçu des commandes les plus utiles et ont pu se faire la main sur quelques TP encadrés par les membres du club. 
-			</p>
-		</div>
+	<div class="paragraphe clearfix">
+		<img class="float-left" src="img/accueil/local.jpg"/>
+		<p>
+			La <span class="surligne">Formation Git</span> a attiré beaucoup de curieux, désireux d'apprendre à maîtriser cet outil indispensable pour coder à plusieurs, mais réputé difficile à prendre en main.
+		</p>
+		<p>
+			Add, commit, push, pull, branch... Les commandes et les traits d'humour se sont enchaînés à un rythme effréné, et autant débutants que confirmés ont trouvé de quoi se mettre sous la dent et développer leurs compétences. 
+		</p>
+	</div>
 
-		<div class="paragraphe clearfix">
-			<img class="float-left" src="img/accueil/local.jpg"/>
-			<p>
-				La Formation Git a attiré beaucoup de curieux, désireux d'apprendre à maîtriser cet outil indispensable pour coder à plusieurs, mais réputé difficile à prendre en main.
-			</p>
-			<p>
-				Add, commit, push, pull, branch... Les commandes et les traits d'humour se sont enchaînés à un rythme effréné, et autant débutants que confirmés ont trouvé de quoi se mettre sous la dent et développer leurs compétences. 
-			</p>
-		</div>
-
-		<div class="paragraphe clearfix">
-			<img class="float-right" src="img/accueil/local.jpg"/>
-			<p>
-				La Formation Web poursuivait deux objectifs : fournir aux élèves les bases nécessaires pour créer un site web de zéro, mais aussi former les futurs développeurs de <a href="upont.php" data-titre="La plateforme uPont - KI">uPont</a>, la plateforme interne des étudiants des Ponts.
-			</p>
-			<p>
-				A raison de deux heures par semaine et d'un TP constituant le fil rouge de la formation, les élèves ont pu découvrir HTML, CSS, ainsi que Javascript et PHP à travers les frameworks Angular et Symfony 2.
-			</p>
-		</div>
-
+	<div class="paragraphe clearfix">
+		<img class="float-right" src="img/accueil/local.jpg"/>
+		<p>
+			La <span class="surligne">Formation Web</span> poursuivait deux objectifs : fournir aux élèves les bases nécessaires pour créer un site web de zéro, mais aussi former les futurs développeurs de <a href="upont.php" data-titre="La plateforme uPont - KI">uPont</a>, la plateforme interne des étudiants des Ponts.
+		</p>
+		<p>
+			A raison de deux heures par semaine et d'un TP constituant le fil rouge de la formation, les élèves ont pu découvrir HTML, CSS, ainsi que Javascript et PHP à travers les frameworks Angular et Symfony 2.
+		</p>
 	</div>
 
 </div>
