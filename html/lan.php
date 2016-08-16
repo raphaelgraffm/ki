c5caf9d80948680a42d7042c89f86a93fb543f2e
raphaelgraffm <raphgraff@live.fr>
2016-08-16 20:53:48 +0200

    Premiere correction de la page Jeux Videos

--- html/lan.php
+++ html/lan.php
@@ -5,7 +5,7 @@
 
 	<p>
 		Tu aimes les jeux vidéos? <span class="surligne">Nous aussi</span>.<br/>
-		Que tu sois plutôt "Qui a deux petites minutes pour un Smash Bros?" ou "Qui a deux petites semaines pour un Civi?", le KI va vite devenir ton nouveau terrain de jeu.
+		Que tu sois plutôt "Qui a deux petites minutes pour un Smash Bros ?" ou "Qui a deux petites semaines pour un Civ V ?", le KI va vite devenir ton nouveau terrain de jeu.
 	</p>
 
 	<h2 class="sous-titre"><span>LA WII U</span></h2>
@@ -23,14 +23,14 @@
 	<h2 class="sous-titre"><span>LES SOIREES JEUX VIDEOS</span></h2>
 
 	<p>
-		Ah, la belle époque où les gamers, les vrais, se retrouvaient ensemble dans une même pièce avec des potes et des pizzas pour se donner des coups de main et de coude! <!-- <span class="akbar" style="font-size:90%;">(zeugma!)</span> -->
+		Ah, la belle époque où les gamers, les vrais, se retrouvaient ensemble dans une même pièce avec des potes et des pizzas pour se donner des coups de main et de coude ! <!-- <span class="akbar" style="font-size:90%;">(zeugma!)</span> -->
 	</p>
 
 	<p>
 		Si comme nous, tu trouves que ton reflet dans l'écran de l'ordinateur est devenu un tantinet prévisible à la longue, le KI organise <span class="surligne">une à deux fois par mois</span> des soirées dédiées entièrement au jeu vidéo. 
 	</p>
 
-	<p class="question">"On jouera à quels jeux? Je peux ramener les miens ?"</p>
+	<p class="question">"On jouera à quels jeux ? Je peux ramener les miens ?"</p>
 
 	<p>
 		Age of Empires II, Counter-Strike, Smash Bros... Peu importe le flacon, tant qu'il y a l'ivresse. <span class="surligne">Fais-nous partager tes coups de coeur</span>, éblouis-nous de tes plus belles découvertes, et rentre chez toi avec des idées plein la tête, des étoiles plein les yeux, et des bonbons plein le ventre.
